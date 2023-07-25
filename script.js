17b11e64ee6d7609f529021e36d2b6c7139154f3
Md Faisal Siddiqui <122595165+Faix321@users.noreply.github.com>
2023-07-26 01:08:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "5a1970f7e30746c9a280ecbbedf316d1";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
